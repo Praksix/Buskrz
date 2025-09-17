a4260369fa750078ad355f9923277151accc8501
Praksix <nathan.vianey@gmail.com>
2025-09-17 16:22:49 +0200

    Ajout des fonctionnalités de gestion des concerts : création des classes Concert, ConcertController, ConcertService, ConcertServiceImpl et ConcertRepository avec les endpoints nécessaires pour le CRUD et la récupération des concerts par artiste, ville et lieu.

---
+++ api/src/main/java/com/praksix/buskrz/repository/ConcertRepository.java
@@ -0,0 +1,19 @@
+package com.praksix.buskrz.repository;
+
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.praksix.buskrz.model.Concert;
+
+@Repository
+public interface ConcertRepository extends MongoRepository<Concert, String> {
+    
+    // Méthodes personnalisées basées sur les propriétés du modèle Artiste
+    public Concert findByArtisteIdsContaining(String artisteId);
+    public List<Concert> findAllByArtisteIdsContaining(String artisteId);
+    public List<Concert> findAllByLieuId(String lieuId);
+    public List<Concert> findAllByDate(String date);
+    public List<Concert> findAllByCity(String city);
+
+}
---
+++ api/src/main/java/com/praksix/buskrz/service/ConcertService.java
@@ -0,0 +1,25 @@
+package com.praksix.buskrz.service;
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.praksix.buskrz.model.Concert;
+
+@Service
+public interface ConcertService {
+    
+
+    public void createConcert(Concert concert);
+    public void updateConcert(Concert concert);
+    public Collection<Concert> getConcerts();
+    public void deleteConcert(String id);
+    public void deleteAllConcerts();
+    public Optional<Concert> getConcertById(String id);
+    public Collection<Concert> getConcertsByArtisteId(String artisteId);
+    public Collection<Concert> getConcertsByCity(String city);
+    public Collection<Concert> getConcertsByLieuId(String lieuId);
+
+
+
+}
